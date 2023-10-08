0129dd4b1317adf0a8699a1733200a1de4f011cb
Rocky <thebestofrocky@gmail.com>
2023-10-08 17:09:13 +0800

    Create Footer & Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+    <div>
+        <h1>林 兴</h1>
+        <p>Android开发工程师</p>
+        <div>
+            <p>联系方式：135-7036-2046</p>
+            <p>GitHub https://github.com/RockyLin</p>
+            <p>邮箱：thebestofrocky@gmail.com</p> 
+        </div>
+    </div>
+    )
+}
+
+export default Header;

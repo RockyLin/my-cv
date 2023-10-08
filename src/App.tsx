6ec55900174a1b61e92aefd3f1f4eeffd6eac726
Rocky <thebestofrocky@gmail.com>
2023-10-08 17:48:15 +0800

    Add Main component

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,14 @@ import './App.css';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
       <Header/>
 
-      <main>
-        Here are the projects I did in the past few years.
-      </main>
+      <Main/>
 
       <Footer/>
     </div>
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,10 @@
+function Main(){
+    return (
+        <div>
+            <div>Work Experience</div>
+            <div>Education</div>
+        </div>
+    )
+}
+
+export default Main;

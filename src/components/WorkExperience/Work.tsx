1c95033155eadaa53b1f1e6ec0b62086017f66db
Rocky <thebestofrocky@gmail.com>
2023-10-09 11:10:53 +0800

    Add Work component

---
+++ src/components/WorkExperience/Work.tsx
@@ -0,0 +1,16 @@
+import "./Work.css"
+
+function Work() {
+    return(
+        <div>
+            <div>Social Club</div>
+
+            <div>Penta</div>
+
+            <div>深圳乐逗游戏有限公司</div>
+
+        </div>
+    );
+}
+
+export default Work;

069d7e8dbbf0ff718bfaf581a8aea84f45da08c8
Rocky <thebestofrocky@gmail.com>
2023-10-09 11:10:37 +0800

    Add Education component

---
+++ src/components/Education/Education.tsx
@@ -0,0 +1,12 @@
+import "./Education.css"
+
+function Education(){
+    return(
+        <div>
+            <h1>学历</h1>
+            <p>中南大学 统招本科 电子信息科学与技术</p>
+        </div>
+    );
+}
+
+export default Education;

65d723841aaea132b62c0778d66273166e15455f
Rocky <thebestofrocky@gmail.com>
2023-10-09 10:59:50 +0800

    Add  Skills component

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -3,15 +3,15 @@ import "./Skills.css"
 function Skills() {
     return (
     <div className="skills-container">
-        <div>Android</div>
-        <div>iOS</div>
-        <div>React</div>
-        <div>Objective-C</div>
-        <div>Java</div>
-        <div>JavaScript</div>
-        <div>Html</div>
-        <div>XML</div>
-        <div>CSS</div>
+        <div className="skills-item">Android</div>
+        <div className="skills-item">iOS</div>
+        <div className="skills-item">React</div>
+        <div className="skills-item">Objective-C</div>
+        <div className="skills-item">Java</div>
+        <div className="skills-item">JavaScript</div>
+        <div className="skills-item">Html</div>
+        <div className="skills-item">XML</div>
+        <div className="skills-item">CSS</div>
     </div>
     )
 }
